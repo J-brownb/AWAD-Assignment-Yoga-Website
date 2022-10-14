df4e23282bf04c17c631280df44fb7d7f38ad9f4
Jonny Hodgson <s223358@uos.ac.uk>
2022-10-14 14:29:53 +0100

    New API + Style

--- js/home.js
+++ js/home.js
@@ -1,81 +1,24 @@
-let muscle = "biceps";
-
+//Searching
 const settings = {
   async: true,
   crossDomain: true,
-  url:
-    "https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=biceps" +
-    muscle,
+  url: "https://lightning-yoga-api.herokuapp.com/yoga_poses",
   method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "93a1747e2cmsh37117e509704babp13eb9fjsn4861d9822a66",
-    "X-RapidAPI-Host": "exercises-by-api-ninjas.p.rapidapi.com",
-  },
 };
 
-const print = function () {
-  $.ajax(settings).done(function (response) {
-    for (let i = 0; i < 3; i++) {
-      let gif = response[i].gifUrl;
-      $("#playground").append(response[i].name);
-      // $("#playground").append(`<img src=${gif}>`);
-    }
-  });
-};
+$.ajax(settings).done(function (response) {
+  for (let i = 0; i < response.items.length; i++) {
+    let img = response.items[i].img_url;
+    let all =
+      `<li>` +
+      `<img src=${img} width="250" height="250">` +
+      `<br>` +
+      `<strong>English Name:</strong> ${response.items[i].english_name} ` +
+      `<br>` +
+      `<strong>Sanskrit Name:</strong> ${response.items[i].sanskrit_name} ` +
+      `<br>` +
+      `</li>`;
 
-// const catSettings = {
-//   async: true,
-//   crossDomain: true,
-//   url: "https://exercisedb.p.rapidapi.com/exercises/bodyPart/back",
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Key": "93a1747e2cmsh37117e509704babp13eb9fjsn4861d9822a66",
-//     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-//   },
-// };
-
-// // //This works to query one at pos 0, 1 result
-// // $.ajax(settings).done(function (response) {
-// //   console.log(response);
-// //   let gif = response[0].gifUrl;
-// //   console.log(gif);
-// //   $("#playground").append(response[0].equipment);
-// //   $("#playground").append(`<img src=${gif}>`);
-// // });
-
-// //This works to get all from one category - category page sorted!
-// const catSearch = function () {
-//   $.ajax(catSettings).done(function (response) {
-//     for (let i = 0; i < 3; i++) {
-//       let gif = response[i].gifUrl;
-//       $("#playground").append(response[i].name);
-//       $("#playground").append(`<img src=${gif}>`);
-//     }
-//   });
-// };
-
-// catSearch();
-
-// //Searching
-// const indSettings = {
-//   async: true,
-//   crossDomain: true,
-//   url: "https://exercisedb.p.rapidapi.com/exercises/name/",
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Key": "93a1747e2cmsh37117e509704babp13eb9fjsn4861d9822a66",
-//     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-//   },
-// };
-
-// $("#exerciseSearchBtn").on("click", function () {
-//   $.ajax(indSettings).done(function (response) {
-//     let search = $("#searching").val();
-//     // $("#playground").append(response[i].name);
-//     for (let i = 0; i < 3; i++) {
-//       let gif = response[i].gifUrl;
-//       $("#playground").append(response[i].name);
-//       $("#playground").append(`<img src=${gif}>`);
-//     }
-//   });
-// });
+    $("#playgroundResults").append(all);
+  }
+});
