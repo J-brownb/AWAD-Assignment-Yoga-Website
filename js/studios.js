79d0d0f676fdaca6eac5f87c809d0d1357f46670
Jonny Hodgson <94555970+J-brownb@users.noreply.github.com>
2022-10-26 21:38:13 +0100

    Api

--- js/studios.js
+++ js/studios.js
@@ -1,35 +1,3 @@
-// //Map
-// var map;
-// var service;
-// var infowindow;
-
-// function initialize() {
-//   var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
-
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: pyrmont,
-//     zoom: 15,
-//   });
-
-//   var request = {
-//     location: pyrmont,
-//     radius: "500",
-//     query: "restaurant",
-//   };
-
-//   service = new google.maps.places.PlacesService(map);
-//   service.textSearch(request, callback);
-// }
-
-// function callback(results, status) {
-//   if (status == google.maps.places.PlacesServiceStatus.OK) {
-//     for (var i = 0; i < results.length; i++) {
-//       var place = results[i];
-//       createMarker(results[i]);
-//     }
-//   }
-// }
-
 //Results
 let url =
   "https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyCYqnXeh46RRRThzx47Cg0J0DH16Jxcp2Q&query=yoga-studios-in-ipswich";
