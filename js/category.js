406e4c2b7045e004476d87427fb36ed1b681345f
Jonny Hodgson <s223358@uos.ac.uk>
2022-12-01 13:57:35 +0000

    attemp

--- js/category.js
+++ js/category.js
@@ -1,6 +1,51 @@
-let url = "https://lightning-yoga-api.herokuapp.com/yoga_categories";
-let catName =
-  "https://lightning-yoga-api.herokuapp.com/yoga_poses?yoga_category_name=";
+//Api Options
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "93a1747e2cmsh37117e509704babp13eb9fjsn4861d9822a66",
+    "X-RapidAPI-Host": "exercises2.p.rapidapi.com",
+  },
+};
+let APIurl = "https://exercises2.p.rapidapi.com/?count=20";
+
+//Api Call All Results
+function apiCall(APIurl, callCount) {
+  fetch(APIurl, options)
+    .then((response) => response.json())
+    .then((response) => {
+      let poses = document.getElementById("category");
+      let count = callCount;
+      for (let i = 0; i < count; i++) {
+        let bodyParts = [];
+        bodyParts.push(response[i].bodyPart);
+        console.log(bodyParts);
+        let img = response[i].image;
+        let all =
+          `<li>` +
+          `<img src=${img} width="200" height="200">` +
+          `<br>` +
+          `<br>` +
+          `<strong>Exercise Name:</strong> ${response[i].name} ` +
+          `<br>` +
+          `<strong>Body Part Focus:</strong> ${response[i].bodyPart} ` +
+          `<br>` +
+          `<strong>Muscle Target:</strong> ${response[i].muscleTarget} ` +
+          `</li>`;
+        poses.innerHTML += all;
+        loading.innerHTML = " ";
+      }
+    });
+}
+apiCall(APIurl, 20);
+
+//Loading Element
+let loadingIcon = `<div class="d-flex justify-content-center" id="spinner">
+<div class="spinner-border" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+</div>`;
+let loading = document.getElementById("loadingarea");
+loading.innerHTML = loadingIcon;
 
 //Dropdown menu
 let dropdown = document.getElementById("dropdownmenu");
@@ -8,52 +53,44 @@ let menuItems = document.getElementById("menuitems");
 dropdown.addEventListener("click", function () {
   menuItems.style.display = "block";
 });
-
 dropdown.addEventListener("mouseleave", function () {
   menuItems.style.display = "none";
 });
 
-//Loading Icon
-let loadingIcon = `<div class="d-flex justify-content-center" id="spinner">
-<div class="spinner-border" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-</div>`;
-let loading = document.getElementById("loadingarea");
-loadingarea.innerHTML = loadingIcon;
+let catName = "https://exercises2.p.rapidapi.com/?bodyPart=";
 
-function displayResults(url) {
-  fetch(url)
-    .then((response) => response.json())
-    .then(function (results) {
-      let poses = document.getElementById("category");
-      for (let i = 0; i < 12; i++) {
-        //img
-        let img = results.items[i].yoga_poses[1].img_url;
-        //url
-        let catUrl =
-          "http://127.0.0.1:5501/yoga-categories/core-yoga-poses.html";
-        //rest of data
-        let all =
-          `<li>` +
-          `<img src=${img} width="200" height="200">` +
-          `<br>` +
-          `</h3>` +
-          `<br>` +
-          `We have ${results.items[i].yoga_poses.length} ` +
-          `${results.items[i].name}` +
-          ` for you to try!` +
-          `<br>` +
-          `<br>` +
-          `<a href="${catUrl}">` +
-          `View more poses in this category` +
-          `</a>` +
-          `</li>`;
-        //Append info to results
-        poses.innerHTML += all;
-        loadingarea.innerHTML = " ";
-      }
-    });
-}
+//old / not working
 
-displayResults(url);
+//       let poses = document.getElementById("category");
+//       for (let i = 0; i < 12; i++) {
+//         //img
+//         let img = results.items[i].yoga_poses[1].img_url;
+//         //url
+//         let catUrl =
+//           "http://127.0.0.1:5501/yoga-categories/core-yoga-poses.html";
+//         //rest of data
+//         let all =
+//           `<li>` +
+//           `<img src=${img} width="200" height="200">` +
+//           `<br>` +
+//           `</h3>` +
+//           `<br>` +
+//           `We have ${results.items[i].yoga_poses.length} ` +
+//           `${results.items[i].name}` +
+//           ` for you to try!` +
+//           `<br>` +
+//           `<br>` +
+//           `<a href="${catUrl}">` +
+//           `View more poses in this category` +
+//           `</a>` +
+//           `</li>`;
+//         //Append info to results
+//         poses.innerHTML += all;
+//         loadingarea.innerHTML = " ";
+//       }
+//     });
+// }
+
+// displayResults(url);
+
+// let url = "https://lightning-yoga-api.herokuapp.com/yoga_categories";
--- js/home.js
+++ js/home.js
@@ -1,10 +1,45 @@
-let url = "https://lightning-yoga-api.herokuapp.com/yoga_poses";
-//Loading Icon
+//Api Options
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "93a1747e2cmsh37117e509704babp13eb9fjsn4861d9822a66",
+    "X-RapidAPI-Host": "exercises2.p.rapidapi.com",
+  },
+};
+let APIurl = "https://exercises2.p.rapidapi.com/?count=20";
+
+//Api Call All Results
+function apiCall(APIurl, callCount) {
+  fetch(APIurl, options)
+    .then((response) => response.json())
+    .then((response) => {
+      let poses = document.getElementById("results");
+      let count = callCount;
+      for (let i = 0; i < count; i++) {
+        let img = response[i].image;
+        let all =
+          `<li>` +
+          `<img src=${img} width="200" height="200">` +
+          `<br>` +
+          `<strong>Exercise Name:</strong> ${response[i].name} ` +
+          `<br>` +
+          `<strong>Body Part Focus:</strong> ${response[i].bodyPart} ` +
+          `<br>` +
+          `<strong>Muscle Target:</strong> ${response[i].muscleTarget} ` +
+          `</li>`;
+        poses.innerHTML += all;
+        loading.innerHTML = " ";
+      }
+    });
+}
+apiCall(APIurl, 4);
+
+//Loading Element
 let loadingIcon = `<div class="d-flex justify-content-center" id="spinner">
-<div class="spinner-border" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-</div>`;
+  <div class="spinner-border" role="status">
+    <span class="sr-only">Loading...</span>
+  </div>
+  </div>`;
 let loading = document.getElementById("loadingarea");
 loading.innerHTML = loadingIcon;
 
@@ -14,66 +49,67 @@ let menuItems = document.getElementById("menuitems");
 dropdown.addEventListener("click", function () {
   menuItems.style.display = "block";
 });
-
 dropdown.addEventListener("mouseleave", function () {
   menuItems.style.display = "none";
 });
 
 //Load more poses
-let clicked = false;
 let loadMore = document.getElementById("loadmore");
 let loadLess = document.getElementById("loadfewer");
 loadMore.addEventListener("click", function () {
-  clicked = !clicked;
+  let APIurl = "https://exercises2.p.rapidapi.com/?count=20";
   loadMore.classList.add("hidden");
   loadLess.classList.remove("hidden");
   poses.innerHTML = "";
   document.documentElement.scrollTop = 60;
-
-  displayResults(url);
+  apiCall(APIurl, 20);
 });
 
 //Load fewer poses
 loadLess.addEventListener("click", function () {
-  clicked = !clicked;
   poses.innerHTML = "";
-  displayResults(url);
+  apiCall(APIurl, 4);
   loadMore.classList.remove("hidden");
   loadLess.classList.add("hidden");
   document.documentElement.scrollTop = 60;
 });
 
-//Display All
-function displayResults(url) {
-  fetch(url)
-    .then((response) => response.json())
-    .then(function (results) {
-      let poses = document.getElementById("results");
-      let count = clicked ? `${results.items.length}` : 4;
-      for (let i = 0; i < count; i++) {
-        let img = results.items[i].img_url;
-        let all =
-          `<li>` +
-          `<img src=${img} width="200" height="200">` +
-          `<br>` +
-          `<strong>English Name:</strong> ${results.items[i].english_name} ` +
-          `<br>` +
-          `<strong>Sanskrit Name:</strong> ${results.items[i].sanskrit_name} ` +
-          `</li>`;
-        poses.innerHTML += all;
-        loading.innerHTML = " ";
-      }
-    });
-}
-displayResults(url);
-
-//Search functionality
+//Search functionality - searches muscle target
 let searchBtn = document.getElementById("exerciseSearchBtn");
 let poses = document.getElementById("results");
 searchBtn.addEventListener("click", function () {
   poses.innerHTML = "";
   let searchTerm = document.getElementById("searching").value;
-  let url = "https://lightning-yoga-api.herokuapp.com/yoga_poses";
-  url += "?english_name=" + searchTerm;
-  displayResults(url);
+  let APIurl =
+    `https://exercises2.p.rapidapi.com/?muscleTarget=` +
+    `${searchTerm}` +
+    `&count=20`;
+  apiCall(APIurl, 20);
+  console.log(APIurl);
 });
+
+// //Category autocomplete
+// var search_terms = ["biceps"];
+
+// function autocompleteMatch(input) {
+//   if (input == "") {
+//     return [];
+//   }
+//   var reg = new RegExp(input);
+//   return search_terms.filter(function (term) {
+//     if (term.match(reg)) {
+//       return term;
+//     }
+//   });
+// }
+
+// function showResults(val) {
+//   res = document.getElementById("result");
+//   res.innerHTML = "";
+//   let list = "";
+//   let terms = autocompleteMatch(val);
+//   for (i = 0; i < terms.length; i++) {
+//     list += "<li>" + terms[i] + "</li>";
+//   }
+//   res.innerHTML = "<ul>" + list + "</ul>";
+// }
