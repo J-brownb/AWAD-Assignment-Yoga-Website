758ab2c120d57c66d1f8344828ad756c18826af1
Jonny Hodgson <s223358@uos.ac.uk>
2022-12-02 11:10:42 +0000

    w

--- js/weatherAPI.js
+++ js/weatherAPI.js
@@ -11,36 +11,53 @@ loading.innerHTML = loadingIcon;
 let weatherUrl =
   "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/ipswich?unitGroup=metric&key=SNJXA2TKPSX2QQLTQTH6SS6LY&contentType=json";
 
-fetch(weatherUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    let weather = document.getElementById("weather");
-    for (let i = 0; i < 14; i++) {
-      let all =
-        `<li>` +
-        `<img src=/images/${data.days[i].icon}.png width="50" height="50">` +
-        `<br>` +
-        `${data.days[i].datetime}` +
-        `<br>` +
-<<<<<<< Updated upstream
-        `<strong>` +
-        `${data.days[i].conditions}` +
-        `</strong>` +
-        `<br>` +
-=======
->>>>>>> Stashed changes
-        `${data.days[i].temp}c` +
-        `<br>` +
-        `${data.days[i].description}` +
-        `<br>` +
-        `</li>`;
-      console.log(data.days[i].datetime);
-      weather.innerHTML += all;
-<<<<<<< Updated upstream
-      loading.innerHTML = "";
-    }
-=======
-    }
-    loading.innerHTML = "pog";
->>>>>>> Stashed changes
-  });
+//Intro
+
+//Function to show weekly forecast
+function weatherCall(count, divID, iCount) {
+  fetch(weatherUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      let weather = document.getElementById(divID);
+      for (let i = iCount; i < count; i++) {
+        let sunrise = `${data.days[i].sunrise}`;
+        let sunriseShort = sunrise.slice(0, 5);
+        let sunset = `${data.days[i].sunset}`;
+        let sunsetShort = sunset.slice(0, 5);
+        let all =
+          `<li>` +
+          `<div class ="date">` +
+          `<h3>` +
+          `${data.days[i].datetime}` +
+          `</h3>` +
+          `</div>` +
+          `<strong>` +
+          `${data.days[i].conditions}` +
+          `</strong>` +
+          `<br>` +
+          `<br>` +
+          `<img src=/images/${data.days[i].icon}.png width="50" height="50">` +
+          `<br>` +
+          `<br>` +
+          `🌡️ ${data.days[i].temp}c` +
+          ` |` +
+          ` 🌧️ ${data.days[i].precipprob}%` +
+          ` |` +
+          `💨 ${data.days[i].windspeed}mph` +
+          `<br>` +
+          `☀️ ${sunriseShort}AM` +
+          ` |` +
+          `🌕 ${sunsetShort}PM` +
+          `<hr>` +
+          `${data.days[i].description}` +
+          `<br>` +
+          `</li>`;
+        divID.innerHTML += all;
+        loading.innerHTML = "";
+      }
+    });
+}
+weatherCall(7, weather, 1);
+
+//Call function to show current day
+weatherCall(1, single, 0);
