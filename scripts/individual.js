d0134a1d8334e8a25c681819e5985d4c6356f1fb
Jonny Hodgson <s223358@uos.ac.uk>
2022-12-11 20:55:51 +0000

    Toast

---
+++ scripts/individual.js
@@ -0,0 +1,8 @@
+//Toast
+document.getElementById("add").onclick = function () {
+  let toastElList = [].slice.call(document.querySelectorAll(".toast"));
+  let toastList = toastElList.map(function (toastEl) {
+    return new bootstrap.Toast(toastEl);
+  });
+  toastList.forEach((toast) => toast.show());
+};
